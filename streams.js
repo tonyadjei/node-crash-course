6418d9cfad0804c05e1ed15df12bd7200514ecff
Anthony Teye-Adjei <tonyteyeadjei@gmail.com>
2021-04-27 20:01:20 +0200

    Revert "basic routing with node, res and req, http server,  3rd party packages, lodash, installing express"
    This reverts commit 621fb862c18df6ad6c9a7b130638c212b2f35ef0.
    
    reverting commit in order to remove the node_modules

--- streams.js
+++ streams.js
@@ -10,16 +10,16 @@ let i = 0;
 const readStream = fs.createReadStream('./docs/blog3.txt', { encoding: 'utf-8' });
 const writeStream = fs.createWriteStream('./docs/blog4.txt');
 
-// in the line below, we place an event listener on our stream object, specifically we listen for the 'data' event. Everytime we get a new buffer(package of data), the callback function is run
-// in the callback function, we get access to the buffer.
-readStream.on('data', (chunk) => {
-    console.log('--------------NEW CHUNK ' + i + ' ---------------')
-    // console.log(chunk.toString());
-    console.log(chunk);
-    writeStream.write('\nNEW CHUNK\n');
-    writeStream.write(chunk);
-    i += 1;
-})
+//in the line below, we place an event listener on our stream object, specifically we listen for the 'data' event. Everytime we get a new buffer(package of data), the callback function is run
+//in the callback function, we get access to the buffer.
+// readStream.on('data', (chunk) => {
+//     console.log('--------------NEW CHUNK ' + i + ' ---------------')
+//     // console.log(chunk.toString());
+//     console.log(chunk);
+//     writeStream.write('\nNEW CHUNK\n');
+//     writeStream.write(chunk);
+//     i += 1;
+// })
 
 //piping: piping is meant to make our lives easier by providing a much faster way of passing data between a readable stream and a writable stream
 readStream.pipe(writeStream);
