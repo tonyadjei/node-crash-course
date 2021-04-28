aa9178e909092f5602751d2ac6c4ff5a7921244c
Anthony Teye-Adjei <tonyteyeadjei@gmail.com>
2021-04-28 20:24:08 +0200

    middleware, 3rd-party middleware, serving static files, built-in express middleware, next() method

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 
+// using a 3rd party middleware: morgan is a logger middleware for node.js
+const morgan = require('morgan');
+
 // create an express app
 
 const app = express();
@@ -10,6 +13,32 @@ app.set('view engine', 'ejs') //app.set() is a method for configuring the web ap
 // listen for requests
 
 app.listen(3000);
+
+// using morgan(3rd party middleware package installed from npm)
+app.use(morgan('dev'));
+
+// there is also middleware that comes built-in with express.
+// an example is the static middleware. when we have static files like css or images, we cannot access them in the front-end(the browser will not have access to them)
+//this is because, our server(node/express) protects all our files and prevents the browser from accessing any file at all
+// we therefore have to indicate which static files we want to server to the browser or make public to the browser for access.
+//to do this, we use the static middleware that comes with express
+
+app.use(express.static('public')); // here, we indicate which folder we are making public and that will contain our static files. the browser has access to all files inside this folder, e.g. 'localhost:3000/styles.css' will display the css file to the browser
+
+
+
+// app.use((req, res, next) => {
+//     console.log('new request made:');
+//     console.log('host: ', req.hostname);
+//     console.log('path: ', req.path);
+//     console.log('method: ', req.method);
+    // next(); express does not know where to go next after a middleware has been executed, hence we have to tell express to continue down with a function that we get access to.
+// });
+
+// app.use((req, res, next) => {
+//     console.log('in the next middleware');
+//     next();
+// })
 
 // note: once a route handler sends a response to the browser, code execution does not continue for the other route handlers
 app.get('/', (req, res) => {
@@ -40,6 +69,7 @@ app.get('/about-us', (req, res) => {
 
 // 404 page
 // app.use() is a method that runs everytime a request comes in(every kind of request, including post requests). But it will not run if code execution does not reach it(that is, if there has already been a match with an existing route handler)
+//this is because, anytime a response is sent, code execution will not continue down to the other middleware if a response was sent by that previous middleware.(middleware is any code that runs(on the server) between receiving a request and sending a response).
 // it's position is important, it must always be placed below all your route handlers that go to actual pages.
 // also, express does not automatically add status codes when we use app.use(), so we have to set it manually, we can do this by using the res.status() method and pass in an integer representing the status code
 // note: res.status() returns the response (res) object. This allows us to perform method chaining. res.status(200) sets the status code.
