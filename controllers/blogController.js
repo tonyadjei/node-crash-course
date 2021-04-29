514c7054face112f217b3644b52af3aaf260525e
Anthony Teye-Adjei <tonyteyeadjei@gmail.com>
2021-04-29 21:58:34 +0200

    implemented MVC pattern by adding controllers(extracting the route handler logic to controller files), modularized the 'views' folder even more

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -4,7 +4,7 @@ const Blog = require('../models/blog');
 const blog_index = (req, res) => {
     Blog.find().sort({ createdAt: -1 }) // we tag along a sort() method to sort the documents. We are sorting by the 'createdAt' property, the -1 means descending order.
     .then((result) => {
-        res.render('index', { title: 'All Blogs', blogs: result })
+        res.render('blogs/index', { title: 'All Blogs', blogs: result })
     })
     .catch((err) => {
         console.log(err);
@@ -16,7 +16,7 @@ const blog_details = (req, res) => {
     // console.log(id);
     Blog.findById(id)
         .then((result) => {
-            res.render('details', { blog: result, title: 'Blog Details' });
+            res.render('blogs/details', { blog: result, title: 'Blog Details' });
         })
         .catch((err) => {
             console.log(err);
@@ -24,7 +24,7 @@ const blog_details = (req, res) => {
 }
 
 const blog_create_get = (req, res) => {
-    res.render('create', { title: 'Create a new Blog' });
+    res.render('blogs/create', { title: 'Create a new Blog' });
 }
 
 const blog_create_post = (req, res) => {
@@ -38,6 +38,17 @@ const blog_create_post = (req, res) => {
         });
 }
 
+const blog_delete = (req, res) => {
+    const id = req.params.id;
+    Blog.findByIdAndDelete(id)
+        .then((result) => {
+            res.json({ redirect: '/blogs' });
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+}
+
 module.exports = {
-    blog_index, blog_details, blog_create_get, blog_create_post
+    blog_index, blog_details, blog_create_get, blog_create_post, blog_delete
 }
--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,35 +1,19 @@
 const express = require('express');
+const Blog = require('../models/blog')
 const blogController = require('../controllers/blogController');
+
 // create new instance of express router object
 const router = express.Router(); // this is like a mini-app but it works inside a main app. we are attaching our route handlers to this 'router' object now.
 
 router.get('/', blogController.blog_index);
 
-router.post('/', (req, res) => {
-    // console.log(req.body);
-    const blog = new Blog(req.body);
-    blog.save()
-        .then((result) => {
-            res.redirect('/blogs'); //here we can redirect the user because the request is coming from a web form. we can also redirect requests made in the browser's addrses bar. But in node.js, we cannot make a redirect when we receive an ajax request.(a request done on the front-end with javascript(with fetch api))
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-});
+router.post('/', blogController.blog_create_post);
 // the route handler below was not working because it was below another get handler that used a route parameter. so it made this one behave as if it was coming in as a route parameter. always check to see that your handler is not being stopped by a route parameter.
 router.get('/create', blogController.blog_create_get);   
 
 router.get('/:id', blogController.blog_details);
 
-router.delete('/:id', (req, res) => {
-    const id = req.params.id;
-    Blog.findByIdAndDelete(id)
-        .then(result => {
-            // here, we cannot redirect the user to another page like the blogs page because the request was done on the front-end with javascrip(an ajax request) and was not done with a web form or anything like that
-            res.json({ redirect: '/blogs' }) // we can send JSON data to the browser using res.json(), res.json() takes in a javascript object, but it will later parse it into JSON format
-        })
-        .catch(err => console.log(err));
-})
+router.delete('/:id', blogController.blog_delete);
 
 
 
